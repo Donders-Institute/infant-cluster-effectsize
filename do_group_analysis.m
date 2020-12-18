dcf24a2be2c56ee6705d80498cdb9bf43d01dbaf
DidiLamers <didilamers@gmail.com>
2020-12-18 14:37:04 +0100

    Now with correct neighbours struct and new statistics for repetitions

--- do_group_analysis.m
+++ do_group_analysis.m
@@ -23,11 +23,11 @@ end
 addpath(data_dir)
 cd(data_dir)
 
-if exist(output_dir, 'dir')
-    rmdir(output_dir, 's');    
+if ~exist(output_dir, 'dir')
+    mkdir(output_dir);  
 end
 
-mkdir(output_dir);
+
 
 %% We loop through all subjects and obtain the results of their timelock analysis
 
@@ -121,8 +121,7 @@ stat_expected_unexpected  = ft_timelockstatistics(cfg, expected_all{:}, unexpect
 
 cfg                       = [];
 cfg.style                 = 'blank';
-% cfg.layout                = 'EEG1010.lay';
-cfg.layout                = 'xyz_layout_1020_33Ch.mat';
+cfg.layout                = 'EEG1010.lay';
 cfg.highlight             = 'on';
 cfg.highlightchannel      = find(stat_expected_unexpected.mask);
 cfg.comment               = 'no';
@@ -136,18 +135,146 @@ savefig(gcf, fullfile(output_dir, 'topoplot_stat_expected_unexpected'));
 
 % First we need to find neighbouring channels
 
-cfg                       = [];
-cfg.channel               = 'EEG'; 
-cfg.method                = 'triangulation';  
-cfg.layout                = 'mylayout_32Ch_actiCAP.mat';              
-cfg.feedback              = 'yes';                           
-neighbours                = ft_prepare_neighbours(cfg, grandavg_expected); % define neighbouring channels
+% This was done with the script below. It is now commented out and we use the struct created from it to continue
 
-% Then we perform the permutation based statistics as well
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% %% To start we read in the channels used in the EEG study
+
+% label = expected.cfg.channel;
+% 
+% % Read sensor positions from the standard 1020 3D format available on fieldtrip
+% elec = ft_read_sens('standard_1020.elc', 'senstype', 'eeg');
+% 
+% if exist('newlayout')
+%     clear newlayout
+% end
+% 
+% % Now we loop through all our used channels, to create a new layout struct
+% % containing only those channels used in our study
+% 
+% for ii = 1:length(label)
+%       index                     = strcmp(label{ii}, elec.label);
+%       newlayout.chanpos(ii, :)  = elec.chanpos(index, :);
+%       newlayout.chantype{ii, 1} = elec.chantype{index};
+%       newlayout.chanunit{ii, 1} = elec.chanunit{index};
+%       newlayout.elecpos(ii, :)  = elec.elecpos(index, :);
+%       newlayout.label{ii, 1}    = elec.label{index};
+%       newlayout.type            = elec.type;
+%       newlayout.unit            = elec.unit;
+% end
+% 
+% % Then we plot this in 3D
+% 
+% fig = ft_plot_sens(newlayout, 'label', 'label');
+% 
+% %% Step 2: draw lines in the figure to connect channels and create a neighbours struct
+% 
+% % We loop through all our channels
+% 
+% for cc = 1:length(newlayout.label)
+%      
+%      % find the corresponding label
+%      channel_to_connect = newlayout.label{cc};
+%          
+%      % We then allow the user to provide a cell array of channels to connect
+%      % the selected channel to
+%      
+%      fprintf('\n')
+%      disp(['channel to connect = ' channel_to_connect ', channel number ' num2str(cc) ' of ' num2str(length(newlayout.label))]);
+%      text = 'provide a row cell array named channels to connect to channel to connect: ';
+%      channels = input(text);       
+%      
+%      % We then create a line between the selected channel and the channels
+%      % to connect that channel to
+%      
+%      connect    = find(strcmp(newlayout.label, channel_to_connect)); 
+%      
+%      for ci = 1:length(channels)
+%          connect1    = find(strcmp(newlayout.label, channels(ci)));
+%          linepoints  = [newlayout.chanpos(connect, :); newlayout.chanpos(connect1, :)];
+%          line(linepoints(:,1), linepoints(:,2), linepoints(:,3));
+%      end
+%  
+%      % Then we create the neighbours struct
+%      
+%      selected_neighbours(cc).label       = channel_to_connect;
+%      selected_neighbours(cc).neighblabel = channels;
+%     
+%  end
+%  
+% % We save the neighbours struct and created image to disk
+% save(fullfile(output_dir, 'selected_neighbours.mat'), 'selected_neighbours');
+% savefig(gcf, fullfile(output_dir, 'selected_neighbours_plot'));
+% 
+% % When this step of the code is finished, comment it out to continue with
+% % manual optimization in the next step
+% 
+% %%  Last step: make some manual changes if necessary
+% 
+% if exist('selected_neighbours.mat')
+%     load('selected_neighbours.mat');
+% end
+% 
+% if exist('selected_neighbours_plot.fig')
+%     openfig('selected_neighbours_plot.fig');
+% end
+% 
+% % Add a line between two points that were missed:
+% 
+% connect     = find(strcmp(newlayout.label, 'Fp1'));
+% connect1    = find(strcmp(newlayout.label, 'Fp2'));
+% linepoints  = [newlayout.chanpos(connect, :); newlayout.chanpos(connect1, :)];
+% line(linepoints(:,1), linepoints(:,2), linepoints(:,3));
+% 
+% % And add the connection to the neighours struct
+% selected_neighbours(1).neighblabel  = [selected_neighbours(1).neighblabel, {'Fp2'}];
+% selected_neighbours(2).neighblabel  = [selected_neighbours(2).neighblabel, {'Fp1'}];
+% 
+% % Then we do a sanity check, make sure that neighbours are fully bidirectional
+% % I.e. if Cz has FCz as a neighbour, then FCz should have Cz as a neighbour
+% 
+% for ii = 1:length(selected_neighbours)
+%     channel = selected_neighbours(ii).label;
+%     for tt  = 1:length(selected_neighbours(ii).neighblabel)
+%         idx = find(strcmp(label, selected_neighbours(ii).neighblabel{tt}));
+%         if sum(strcmp(selected_neighbours(idx).neighblabel, channel))==0
+%           selected_neighbours(idx).neighblabel = [selected_neighbours(idx).neighblabel, {channel}];
+%         end
+%     end
+% end
+% 
+% % And we replot so that all line are now correct
+% 
+% close all
+% 
+% fig = ft_plot_sens(newlayout, 'label', 'label');
+% 
+% for ii = 1:length(selected_neighbours)
+%     channel         = selected_neighbours(ii).label;
+%     connect         = find(strcmp(newlayout.label, channel));
+%     for tt          = 1:length(selected_neighbours(ii).neighblabel)        
+%         connect1    = find(strcmp(newlayout.label, selected_neighbours(ii).neighblabel{tt}));
+%         linepoints  = [newlayout.chanpos(connect, :); newlayout.chanpos(connect1, :)];
+%         line(linepoints(:,1), linepoints(:,2), linepoints(:,3));
+%     end
+% end
+% 
+% % And save this plot and new neighbours
+% 
+% save(fullfile(output_dir, 'selected_neighbours.mat'), 'selected_neighbours');
+% savefig(gcf, fullfile(output_dir, 'selected_neighbours_plot'));
+
+%%  Then we perform the permutation based statistics
+
+if exist([output_dir filesep 'selected_neighbours.mat'], 'file')
+    load([output_dir filesep 'selected_neighbours.mat']);
+end
 
 cfg                       = [];
 cfg.channel               = 'EEG';
-cfg.neighbours            = neighbours; % defined as above
+cfg.neighbours            = selected_neighbours; % defined as above
 cfg.parameter             = 'avg';
 cfg.method                = 'montecarlo';
 cfg.statistic             = 'ft_statfun_depsamplesT';
@@ -177,5 +304,49 @@ figure; ft_topoplotER(cfg, grandavg_expected)
 % And save the data
 save(fullfile(output_dir, 'stat_expected_unexpected_clusstats.mat'), 'stat_expected_unexpected_clusstats');
 savefig(gcf, fullfile(output_dir, 'topoplot_stat_expected_unexpected_clusstats'));
+
+%% Finally we perform clusterstats for the repetitions of expected stimuli
+
+if exist([output_dir filesep 'selected_neighbours.mat'], 'file')
+    load([output_dir filesep 'selected_neighbours.mat']);
+end
+
+cfg                       = [];
+cfg.channel               = 'EEG';
+cfg.neighbours            = selected_neighbours; % defined as above
+cfg.parameter             = 'avg';
+cfg.method                = 'montecarlo';
+cfg.statistic             = 'depsamplesregrT';
+cfg.alpha                 = 0.05;
+cfg.correctm              = 'cluster';
+cfg.correcttail           = 'prob';
+cfg.numrandomization      = 1024; % Enlarge this when doing real analysis
+
+%design matrix
+Nsub                      = length(subjectlist);
+Nrep                      = 3; % three repetitions or "time points" 
+design                    = zeros(2, Nrep*Nsub);
+design(1, :)              = repmat(1:Nsub, 1, Nrep);  % subject
+design(2, :)              = repelem(1:Nrep, Nsub);  % session
+cfg.design                = design;
+cfg.uvar                  = 1;  % the unit variable: subjects
+cfg.ivar                  = 2;  % the independent variable, here number of repetitions
+
+stat_repetitions_clusstats  = ft_timelockstatistics(cfg, repetition1_all{:}, repetition2_all{:}, repetition3_all{:});
+
+% Then plot the result
+
+cfg                       = [];
+cfg.style                 = 'blank';
+cfg.layout                = 'EEG1010.lay';
+cfg.highlight             = 'on';
+cfg.highlightchannel      = find(stat_repetitions_clusstats.mask);
+cfg.comment               = 'no';
+figure; ft_topoplotER(cfg, grandavg_repetition1)
+
+% And save the data
+save(fullfile(output_dir, 'stat_repetitions_clusstats.mat'), 'stat_repetitions_clusstats');
+savefig(gcf, fullfile(output_dir, 'topoplot_stat_repetitions_clusstats'));
+
 
 close all
