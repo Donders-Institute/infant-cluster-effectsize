92419dda97de3d1471d7738ce3578ba0b57b75c8
Marlene Meyer <m.meyer@donders.ru.nl>
2021-04-04 13:43:23 +0200

    Update do_group_analysis.m
    - commented out repetitions for now
    - made topoplot showing results of clusterbased test
    - added average effect size cohen's d
    - added ERP figure of channel with maximum effect

--- do_group_analysis.m
+++ do_group_analysis.m
@@ -50,6 +50,7 @@ subjectlist_new(excluded_participants) = [];
 save(fullfile(output_dir, 'excludedparticipants.mat'), 'excluded_participants');
 
 %% We loop through all subjects and obtain the results of their timelock analysis
+load(fullfile(output_dir, 'excludedparticipants.mat'), 'excluded_participants');
 
 for ii = 1:length(subjectlist_new)
     folder                  = [results filesep subjectlist_new{ii}];
@@ -57,12 +58,12 @@ for ii = 1:length(subjectlist_new)
     expected_all(ii)        = { expected }; % We collect all averages in a cell array of structs
     load([folder filesep 'timelock_unexpected.mat']);
     unexpected_all(ii)      = { unexpected };
-    load([folder filesep 'timelock_repetition1.mat']);
-    repetition1_all(ii)     = { repetition1 };
-    load([folder filesep 'timelock_repetition2.mat']);
-    repetition2_all(ii)     = { repetition2 };
-    load([folder filesep 'timelock_repetition3.mat']);
-    repetition3_all(ii)     = { repetition3 };
+%     load([folder filesep 'timelock_repetition1.mat']);
+%     repetition1_all(ii)     = { repetition1 };
+%     load([folder filesep 'timelock_repetition2.mat']);
+%     repetition2_all(ii)     = { repetition2 };
+%     load([folder filesep 'timelock_repetition3.mat']);
+%     repetition3_all(ii)     = { repetition3 };
 end
 
 %% calculate grand average for the expected and unexpected stimuli
@@ -128,7 +129,7 @@ cfg.correctm              = 'no'; % No multiple comparisons correction for now
 cfg.correcttail           = 'prob'; % distribute the alpha level over both tails by multiplying the probability with a factor two, prior to thresholding it wich cfg.alpha.
 cfg.numrandomization      = 1024; % Enlarge this when doing real analysis
 
-Nsub                      = length(subjectlist);
+Nsub                      = length(subjectlist_new);
 cfg.design(1,1:2*Nsub)    = [ones(1,Nsub) 2*ones(1,Nsub)];
 cfg.design(2,1:2*Nsub)    = [1:Nsub 1:Nsub];
 cfg.ivar                  = 1; % the 1st row in cfg.design contains the independent variable
@@ -285,7 +286,7 @@ savefig(gcf, fullfile(output_dir, 'topoplot_stat_expected_unexpected'));
 % save(fullfile(output_dir, 'selected_neighbours.mat'), 'selected_neighbours');
 % savefig(gcf, fullfile(output_dir, 'selected_neighbours_plot'));
 
-%%  Then we perform the permutation based statistics
+%% Perform cluster-based permutation statistics
 
 load(fullfile(scripts, 'selected_neighbours.mat'));
 
@@ -300,7 +301,7 @@ cfg.correctm              = 'cluster';
 cfg.correcttail           = 'prob';
 cfg.numrandomization      = 1024; % Enlarge this when doing real analysis
 
-Nsub                      = length(subjectlist);
+Nsub                      = length(subjectlist_new);
 cfg.design(1,1:2*Nsub)    = [ones(1,Nsub) 2*ones(1,Nsub)];
 cfg.design(2,1:2*Nsub)    = [1:Nsub 1:Nsub];
 cfg.ivar                  = 1; % the 1st row in cfg.design contains the independent variable
@@ -308,21 +309,210 @@ cfg.uvar                  = 2; % the 2nd row in cfg.design contains the subject 
 
 stat_expected_unexpected_clusstats  = ft_timelockstatistics(cfg, expected_all{:}, unexpected_all{:});
 
-% Then plot the result
-
+%% Plot the results
+% For this purpose, first calculate the difference between conditions
 cfg                       = [];
-cfg.style                 = 'blank';
-cfg.layout                = 'EEG1010.lay';
-cfg.highlight             = 'on';
-cfg.highlightchannel      = find(stat_expected_unexpected_clusstats.mask);
-cfg.comment               = 'no';
-figure; ft_topoplotER(cfg, grandavg_expected)
+cfg.operation             = 'subtract';
+cfg.parameter             = 'avg';
+grandavg_diff_exp_vs_unexp= ft_math(cfg, grandavg_expected, grandavg_unexpected);
 
-% Find the p value of the largest cluster
+% Find clusters with a 5% two-sided cutoff based on the cluster p-values
+pos_cluster_pvals = [stat_expected_unexpected_clusstats.posclusters(:).prob];
+pos_clust = find(pos_cluster_pvals < 0.025);
+pos = ismember(stat_expected_unexpected_clusstats.posclusterslabelmat, pos_clust);
+
+neg_cluster_pvals = [stat_expected_unexpected_clusstats.negclusters(:).prob];
+neg_clust = find(neg_cluster_pvals < 0.025);
+neg = ismember(stat_expected_unexpected_clusstats.negclusterslabelmat, neg_clust);
+
+% % Alternatively, plot only the first positive/negative cluster
+% pos = stat_expected_unexpected_clusstats.posclusterslabelmat ==1;
+% neg = stat_expected_unexpected_clusstats.negclusterslabelmat ==1;
+
+% Set plotting specifications
+timestep = 0.05; % plot every 0.05 sec intervals
+sampling_rate = 500; % set sampling frequency %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% NOTE: make dynamic
+sample_count = length(stat_expected_unexpected_clusstats.time);
+j = [stat_expected_unexpected_clusstats.time(1):timestep:stat_expected_unexpected_clusstats.time(end)];% start of each interval for plotting in seconds
+m = [1:timestep*sampling_rate:sample_count]; % start of each interval for plotting in sample points
+
+[i1,i2] = match_str(grandavg_diff_exp_vs_unexp.label, stat_expected_unexpected_clusstats.label); % matching channel labels
+
+for k = 1:30
+    subplot(6,5,k);
+    cfg = [];
+    cfg.xlim = [j(k) j(k+1)]; % current interval
+    cfg.zlim = [-10 10]; % set minimum and maximum z-axis
+    pos_int = zeros(numel(grandavg_diff_exp_vs_unexp.label),1);
+    neg_int = zeros(numel(grandavg_diff_exp_vs_unexp.label),1);
+    pos_int(i1) = all(pos(i2, m(k):m(k+1)),2); % determine which channel are in a cluster throughout the current time interval (pos cluster)
+    neg_int(i1) = all(neg(i2, m(k):m(k+1)),2); % determine which channel are in a cluster throughout the current time interval (neg cluster)
+    
+    cfg.highlight = 'on';
+    cfg.highlightchannel = find(pos_int | neg_int); % highlight channels belonging to a cluster
+    cfg.highlightcolor     = [1 1 1]; % highlight marker color (default = [0 0 0] (black))
+    cfg.comment = 'xlim';
+    cfg.commentpos = 'title';
+    cfg.layout =  'EEG1010.lay';
+    cfg.interactive = 'no';
+    ft_topoplotER(cfg, grandavg_diff_exp_vs_unexp)
+    
+end
 
 % And save the data
 save(fullfile(output_dir, 'stat_expected_unexpected_clusstats.mat'), 'stat_expected_unexpected_clusstats');
 savefig(gcf, fullfile(output_dir, 'topoplot_stat_expected_unexpected_clusstats'));
+
+%% Determine effect size
+% Calculate Cohen's d for the average difference in the respective cluster and determine at which channel/time this Cohen's d
+% is maximal 
+
+% First for the positive cluster
+effect_window_pos = stat_expected_unexpected_clusstats.posclusterslabelmat==1;
+
+for ii = 1:size(subjectlist_new,1)
+    folder                  = [results filesep subjectlist_new{ii}];
+    load([folder filesep 'timelock_expected.mat']);
+    load([folder filesep 'timelock_unexpected.mat']);
+    
+    a = expected.avg(effect_window_pos);
+    b = unexpected.avg(effect_window_pos);
+    c = a-b;
+    Pos.ERP_Diff_alltimechan(ii,:) =c;
+    Pos.ERP_Diff(ii) = nanmean([a - b]);
+    clear expected unexpected a b c
+
+end
+Pos.stdev_ERP_diff = std(Pos.ERP_Diff);
+Pos.mean_ERP_diff = mean(Pos.ERP_Diff);
+Pos.cohensd_ERP_diff = Pos.mean_ERP_diff/Pos.stdev_ERP_diff;
+
+for t = 1:size(Pos.ERP_Diff_alltimechan,2)   
+    Pos.cohensd_ERP_Diff_alltimechan(t)=(nanmean(Pos.ERP_Diff_alltimechan(:,t)))/(std(Pos.ERP_Diff_alltimechan(:,t)));
+end
+[ Pos.cohensd_ERP_Diff_Max, idx]= max(Pos.cohensd_ERP_Diff_alltimechan);
+
+[Pos.row,Pos.col] = find(stat_expected_unexpected_clusstats.posclusterslabelmat==1);
+ 
+folder                  = [results filesep subjectlist_new{1}]; % load an example participant's ERP for determining channel and time information
+load([folder filesep 'timelock_expected.mat']);
+
+fprintf('\n')
+disp('~~~~~')
+disp(['Contrast: Standard vs. Oddball: ']);
+disp(['Effect size Cohens d of average positive cluster is ' num2str(Pos.cohensd_ERP_diff)])
+disp(['Maximum effect size is ' num2str(Pos.cohensd_ERP_Diff_Max) ' at channel ' expected.label{Pos.row(idx)} ' and at time ' num2str(expected.time(Pos.col(idx))) ' sec']);
+disp('~~~~~')
+fprintf('\n')
+
+save(fullfile(output_dir, 'EffectSize_Pos.mat'), 'Pos');
+
+% Then for the negative clustesr
+effect_window_neg = stat_expected_unexpected_clusstats.negclusterslabelmat==1;
+
+
+for ii = 1:size(subjectlist_new,1)
+    folder                  = [results filesep subjectlist_new{ii}];
+    load([folder filesep 'timelock_expected.mat']);
+    load([folder filesep 'timelock_unexpected.mat']);
+    
+    a = expected.avg(effect_window_neg);
+    b = unexpected.avg(effect_window_neg);
+    c = a-b;
+    Neg.ERP_Diff_alltimechan(ii,:) =c;
+    Neg.ERP_Diff(ii) = nanmean([a - b]);
+    clear expected unexpected a b c
+
+end
+Neg.stdev_ERP_diff = std(Neg.ERP_Diff);
+Neg.mean_ERP_diff = mean(Neg.ERP_Diff);
+Neg.cohensd_ERP_diff = Neg.mean_ERP_diff/Neg.stdev_ERP_diff;
+
+for t = 1:size(Neg.ERP_Diff_alltimechan,2)   
+    Neg.cohensd_ERP_Diff_alltimechan(t)=(nanmean(Neg.ERP_Diff_alltimechan(:,t)))/(std(Neg.ERP_Diff_alltimechan(:,t)));
+end
+[ Neg.cohensd_ERP_Diff_Max, idx]= min(Neg.cohensd_ERP_Diff_alltimechan);
+
+[Neg.row,Neg.col] = find(stat_expected_unexpected_clusstats.negclusterslabelmat==1);
+ 
+folder                  = [results filesep subjectlist_new{1}]; % load an example participant's ERP for determining channel and time information
+load([folder filesep 'timelock_expected.mat']);
+
+fprintf('\n')
+disp('~~~~~')
+disp(['Contrast: Standard vs. Oddball: '])
+disp(['Effect size Cohens d of average positive cluster is ' num2str(Neg.cohensd_ERP_diff)])
+disp(['Maximum effect size is ' num2str(Neg.cohensd_ERP_Diff_Max) ' at channel ' expected.label{Neg.row(idx)} ' and at time ' num2str(expected.time(Neg.col(idx))) ' sec']);
+disp('~~~~~')
+fprintf('\n')
+
+save(fullfile(output_dir, 'EffectSize_Neg.mat'), 'Neg');
+
+%% Plot ERP timecourse of the electrode with the maximal effect size
+
+% Determine variability between participants
+cfg                      = [];
+cfg.channel              = 'all';
+cfg.latency              = 'all';
+cfg.parameter            = 'avg';
+cfg.keepindividual       = 'yes';
+grandavg_expected_all        = ft_timelockgrandaverage(cfg, expected_all{:});
+grandavg_unexpected_all     = ft_timelockgrandaverage(cfg, unexpected_all{:});
+
+se_grandavg_expected = squeeze(nanstd(grandavg_expected_all.individual/sqrt(length(subjectlist_new))));
+se_grandavg_unexpected = squeeze(nanstd(grandavg_unexpected_all.individual/sqrt(length(subjectlist_new))));
+
+% Set color specifications
+colour_code = {'b','g', 'm', 'r', 'c', 'k', ':k', 'g', 'm'};
+shaded_area = {[0, 0, 1],[0, 1, 0],[1 0 1], [1 0 0], [0 1 1], [0, 1, 1],[0, 1, 0],[0, 0, 0],[0, 0, 0]};
+
+% Plot 
+figure;
+% ERP of channel with maximum effect size of positive Cluster
+subplot(1,2,1)
+% Condition 1
+plot(grandavg_expected.time,grandavg_expected.avg(Pos.row(idx),:),colour_code{1}, 'LineWidth', 1.5)
+
+mean_expected = grandavg_expected.avg(Pos.row(idx),:);
+se_expected = se_grandavg_expected(Pos.row(idx),:);
+patch([grandavg_expected.time, fliplr(grandavg_expected.time)], [mean_expected-se_expected, fliplr(mean_expected+se_expected)],  shaded_area{1}, 'edgecolor', 'none', 'FaceAlpha', .3);
+
+hold all
+% Condition 2
+plot(grandavg_unexpected.time,grandavg_unexpected.avg(Pos.row(idx),:),colour_code{4}, 'LineWidth', 1.5)
+
+mean_unexpected = grandavg_unexpected.avg(Pos.row(idx),:);
+se_unexpected = se_grandavg_unexpected(Pos.row(idx),:);
+patch([grandavg_expected.time, fliplr(grandavg_expected.time)], [mean_unexpected-se_unexpected, fliplr(mean_unexpected+se_unexpected)],  shaded_area{4}, 'edgecolor', 'none', 'FaceAlpha', .3);
+
+xlabel('Time [s]');
+ylabel('Amplitude [mV]');
+ylim([-15 15])
+line([-.5 1],[ 0 0], 'Color', [0 0 0],'LineStyle', ':')
+title(['Maximum effect of positive cluster at channel ' grandavg_expected.label(Pos.row(idx))])
+
+% ERP of channel with maximum effect size of positive Cluster
+subplot(1,2,2)
+% Condition 1
+plot(grandavg_expected.time,grandavg_expected.avg(Neg.row(idx),:),colour_code{1}, 'LineWidth', 1.5)
+
+mean_expected = grandavg_expected.avg(Neg.row(idx),:);
+se_expected = se_grandavg_expected(Neg.row(idx),:);
+patch([grandavg_expected.time, fliplr(grandavg_expected.time)], [mean_expected-se_expected, fliplr(mean_expected+se_expected)],  shaded_area{1}, 'edgecolor', 'none', 'FaceAlpha', .3);
+
+hold all
+% Condition 2
+plot(grandavg_unexpected.time,grandavg_unexpected.avg(Neg.row(idx),:),colour_code{4}, 'LineWidth', 1.5)
+
+mean_unexpected = grandavg_unexpected.avg(Neg.row(idx),:);
+se_unexpected = se_grandavg_unexpected(Neg.row(idx),:);
+patch([grandavg_expected.time, fliplr(grandavg_expected.time)], [mean_unexpected-se_unexpected, fliplr(mean_unexpected+se_unexpected)],  shaded_area{4}, 'edgecolor', 'none', 'FaceAlpha', .3);
+
+xlabel('Time [s]');
+ylabel('Amplitude [mV]');
+ylim([-15 15])
+line([-.5 1],[ 0 0], 'Color', [0 0 0],'LineStyle', ':')
+title(['Maximum effect of negative cluster at channel ' grandavg_expected.label(Neg.row(idx))])
 
 %% Finally we perform clusterstats for the repetitions of expected stimuli
 
